d3f8a40bd321febcebb419f5e3eb897fc10966ec
kilpinval <kilpinval@gmail.com>
2025-01-16 02:45:03 +0000

    two_commit

---
+++ siete_sindec.c
@@ -0,0 +1,11 @@
+breria para usar el PIC18F4550
+#fuses HS,WDT,NOPROTECT,PUTNOPBADEN //Configuracion de fusibles
+#use delay(clock=) //Fosc=12MHz
+#BYTE pord_d=0xF83 //Identificador para el uerto d en la localidad 0x83.
+int i;              //Variable i, contador para el bucle FOR.
+const int x[]=,{64,121,36,48,25,18,2,120,0,16}
+
+void main8void)
+{
+
+}
