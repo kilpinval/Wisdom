c62290ce3e2c05b4c79005983b810469b3ca1399
kilpinval <kilpinval@gmail.com>
2025-01-16 12:01:39 +0000

    tree_commit

--- siete_sindec.c
+++ siete_sindec.c
@@ -1,4 +1,4 @@
-breria para usar el PIC18F4550
+#libreria para usar el PIC18F4550
 #fuses HS,WDT,NOPROTECT,PUTNOPBADEN //Configuracion de fusibles
 #use delay(clock=) //Fosc=12MHz
 #BYTE pord_d=0xF83 //Identificador para el puerto d en la localidad 0x83.
@@ -7,19 +7,18 @@ const int x[]={64,121,36,48,25,18,2,120,0,16}
 
 void main(void)
 {
-    set_trisd=0x00;
+    set_tris_d=0x00;
+    disable interrups(GLOBAL);
+    port_d = 64;
+
     while(TRUE)
     {
         for(i=0;i<=9;++i)
+
         {
             delay_ms = 500;
-            pord_d[i]=x[];
-            {
-                /* code */
-            }
-            ]
-
-
+            port_d=x[i];
+        
         }
 
     }
